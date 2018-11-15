04562c433efb3e0dbb263168221671d339dadad4
Alpesh Solanki <im.alpesh13@gmail.com>
2018-11-15 23:39:53 +0530

    reformatted code based on solidity style guide

--- contracts/lobefy_sale.sol
+++ contracts/lobefy_sale.sol
@@ -23,44 +23,44 @@ interface ERC20 {
  */
 library SafeMath {
 
-  /**
-  * @dev Multiplies two numbers, throws on overflow.
-  */
-  function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    if (a == 0) {
-      return 0;
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        if (a == 0) {
+            return 0;
+        }
+        c = a * b;
+        assert(c / a == b);
+        return c;
     }
-    c = a * b;
-    assert(c / a == b);
-    return c;
-  }
 
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
-    return a / b;
-  }
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return a / b;
+    }
 
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
-  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-    assert(b <= a);
-    return a - b;
-  }
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
 
-  /**
-  * @dev Adds two numbers, throws on overflow.
-  */
-  function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
-    c = a + b;
-    assert(c >= a);
-    return c;
-  }
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
 }
 
 
@@ -70,26 +70,26 @@ library SafeMath {
  * functions, this simplifies the implementation of "user permissions".
  */
 contract Ownable {
-  address public owner;
+    address public owner;
   
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
 
-  /**
-   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
-   * account.
-   */
-  constructor() public {
-    owner = msg.sender;
-  }
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    constructor() public {
+        owner = msg.sender;
+    }
 
-  /**
-   * @dev Throws if called by any account other than the owner.
-   */
-  modifier onlyOwner() {
-    require(msg.sender == owner, "required to called by an Owner.");
-    _;
-  }
+    /**
+    * @dev Throws if called by any account other than the owner.
+    */
+    modifier onlyOwner() {
+        require(msg.sender == owner, "required to called by an Owner.");
+        _;
+    }
 
   /**
    * @dev Allows the current owner to transfer control of the contract to a newOwner.
@@ -212,7 +212,7 @@ contract LobefyCrowdsale is Ownable {
     /**
      * @dev fallback function ***DO NOT OVERRIDE***
      */
-    function () external payable {
+    function() external payable {
         revert("Fallback function disabled. Buy tokens from the website");
     }
     
@@ -275,9 +275,9 @@ contract LobefyCrowdsale is Ownable {
         return _soldPhaseFour;
     }
     
-    /**
-     * @dev low level token purchase ***DO NOT OVERRIDE***
-     */
+/**
+ * @dev low level token purchase ***DO NOT OVERRIDE***
+ */
     function buyTokens(address investor, bytes32 messsageHash, bytes signature) public payable onSaleRunning{
         uint256 weiAmount = msg.value;
         _preValidatePurchase(investor, weiAmount, messsageHash, signature);
@@ -295,11 +295,11 @@ contract LobefyCrowdsale is Ownable {
     }
   
   
-    // -----------------------------------------
-    // Internal interface (extensible)
-    // -----------------------------------------
+// -----------------------------------------
+// Internal interface (extensible)
+// -----------------------------------------
     
-    // Pre validation
+// Pre validation
     
     function _preValidatePurchase(address investor, uint256 weiAmount, bytes32 messsageHash, bytes signature) internal view {
         require(investor != address(0), "Invalid address.");
@@ -308,82 +308,83 @@ contract LobefyCrowdsale is Ownable {
         require (ownerAddress == owner, "Signature not matched.");
     }
     
-    // Get token amount and bonuses
+// Get token amount and bonuses
     
     function _getTokenAmount(uint256 weiAmount) internal view returns (uint256) {
         uint256 bonusRate;
-        if (block.timestamp >= phaseOneStart && block.timestamp <=phaseOneEnd) {
-            bonusRate = _rate.add(_rate);                                   // 100% bonus 2xrate
+        if (block.timestamp >= phaseOneStart && block.timestamp <= phaseOneEnd) {
+            bonusRate = _rate.add(_rate);                                               // 100% bonus 2xrate
             return weiAmount.mul(bonusRate);
-        }   else if (block.timestamp >= phaseTwoStart && block.timestamp <=phaseTwoEnd) {
-                bonusRate = _rate.add((_rate.mul(5)).div(10));              // 50% bonus 1.5xrate
-                return weiAmount.mul(bonusRate);
-            }   else if (block.timestamp >= phaseThreeStart && block.timestamp<=phaseThreeEnd) {
-                    bonusRate = _rate.add((_rate.mul(5)).div(20));          // 25% bonus 1.25xrate
-                    return weiAmount.mul(bonusRate);
-                }   else {
-                        return weiAmount.mul(_rate);
-                    }
+        } else if (block.timestamp >= phaseTwoStart && block.timestamp <= phaseTwoEnd) {
+            bonusRate = _rate.add((_rate.mul(5)).div(10));                              // 50% bonus 1.5xrate
+            return weiAmount.mul(bonusRate);
+        } else if (block.timestamp >= phaseThreeStart && block.timestamp<= phaseThreeEnd) {
+            bonusRate = _rate.add((_rate.mul(5)).div(20));                              // 25% bonus 1.25xrate
+            return weiAmount.mul(bonusRate);
+        } else {
+            return weiAmount.mul(_rate);                                                // 0% bonus 1xrate
+        }
     }
     
     
-    // Token transfer
+// Token transfer
 
     function _processPurchase(address investor, uint256 tokenAmount) public {
         _token.transfer(investor, tokenAmount);
     }
     
     
-    // Raised Ether transfer
+// Raised Ether transfer
     
     function _forwardFunds(uint256 weiAmount) internal {
         _wallet.transfer(weiAmount);
     }
     
     
-    // Update statistics
+// Update statistics
     
     function _updateStageStates(uint256 tokens) internal{
         if (block.timestamp >= phaseOneStart && block.timestamp <=phaseOneEnd) {
             _soldPhaseone = _soldPhaseone.add(tokens);
-        }   else if (block.timestamp >= phaseTwoStart && block.timestamp <=phaseTwoEnd) {
-                    _soldPhaseTwo = _soldPhaseTwo.add(tokens);
-            }   else if (block.timestamp >= phaseThreeStart && block.timestamp <=phaseThreeEnd) {
-                        _soldPhaseThree = _soldPhaseThree.add(tokens);
-                }   else{
-                            _soldPhaseFour = _soldPhaseFour.add(tokens);
-                    }
+        } else if (block.timestamp >= phaseTwoStart && block.timestamp <=phaseTwoEnd) {
+            _soldPhaseTwo = _soldPhaseTwo.add(tokens);
+        } else if (block.timestamp >= phaseThreeStart && block.timestamp <=phaseThreeEnd) {
+            _soldPhaseThree = _soldPhaseThree.add(tokens);
+        } else{
+            _soldPhaseFour = _soldPhaseFour.add(tokens);
+        }
     }
     
+// Internal function to recover address of signer from signature and message hash
     
     function recover(bytes32 hash, bytes sig) public pure returns (address) {
-      bytes32 r;
-      bytes32 s;
-      uint8 v;
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
 
-      //Check the signature length
-      if (sig.length != 65) {
-        return (address(0));
-      }
+    //Check the signature length
+        if (sig.length != 65) {
+            return (address(0));
+        }
 
-      // Divide the signature in r, s and v variables
-      assembly {
-        r := mload(add(sig, 32))
-        s := mload(add(sig, 64))
-        v := byte(0, mload(add(sig, 96)))
-      }
+    // Divide the signature in r, s and v variables
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
 
-      // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
-      if (v < 27) {
-        v += 27;
-      }
+    // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
+        if (v < 27) {
+            v += 27;
+        }
 
-      // If the version is correct return the signer address
-      if (v != 27 && v != 28) {
-        return (address(0));
-      } else {
-        return ecrecover(hash, v, r, s);
-      }
+    // If the version is correct return the signer address
+        if (v != 27 && v != 28) {
+            return (address(0));
+        } else {
+            return ecrecover(hash, v, r, s);
+        }
     }
     
     
